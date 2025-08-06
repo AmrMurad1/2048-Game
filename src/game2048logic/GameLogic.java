c9a8da7a0e5708ecfd1c523f3dd2a7cb518a027b
Amr Murad <amrmurad2003@gmail.com>
2025-08-06 16:03:52 +0300

    task 7 implemented

--- src/game2048logic/GameLogic.java
+++ src/game2048logic/GameLogic.java
@@ -1,22 +1,10 @@
 package game2048logic;
 
 import game2048rendering.Side;
-import static game2048logic.MatrixUtils.rotateLeft;
-import static game2048logic.MatrixUtils.rotateRight;
 
 
 public class GameLogic {
 
-    /** Moves the given tile up as far as possible, subject to the minR constraint.
-     *
-     * @param board the current state of the board
-     * @param r     the row number of the tile to move up
-     * @param c -   the column number of the tile to move up
-     * @param minR  the minimum row number that the tile can land in, e.g.
-     *              if minR is 2, the moving tile should move no higher than row 2.
-     * @return      if there is a merge, returns the 1 + the row number where the merge occurred.
-     *              if no merge occurs, then return 0.
-     */
     public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
         if (board[r][c] == 0) return 0;
 
@@ -56,23 +44,24 @@ public class GameLogic {
         }
     }
 
-    /**
-     * Modifies the board to simulate tilting the entire board to
-     * the given side.
-     *
-     * @param board the current state of the board
-     * @param side  the direction to tilt
-     */
+
     public static void tilt(int[][] board, Side side) {
-        // TODO: fill this in in task 7
-        if (side == Side.EAST) {
-            return;
-        } else if (side == Side.WEST) {
-            return;
+        if (side == Side.NORTH) {
+            tiltUp(board);
         } else if (side == Side.SOUTH) {
-            return;
-        } else {
-            return;
+            MatrixUtils.rotateLeft(board);
+            MatrixUtils.rotateLeft(board);
+            tiltUp(board);
+            MatrixUtils.rotateRight(board);
+            MatrixUtils.rotateRight(board);
+        } else if (side == Side.WEST) {
+            MatrixUtils.rotateRight(board);
+            tiltUp(board);
+            MatrixUtils.rotateLeft(board);
+        } else if (side == Side.EAST) {
+            MatrixUtils.rotateLeft(board);
+            tiltUp(board);
+            MatrixUtils.rotateRight(board);
         }
     }
 }
